bd5c2f815ef4a36b5682ee4df12666a832b29307
FearQuare <elmira.aras@protonmail.com>
2026-01-19 21:43:26 +0300

    Settings button style change

--- client/src/components/ProfileCard.jsx
+++ client/src/components/ProfileCard.jsx
@@ -18,7 +18,7 @@ export default function ProfileCard({ userData, setUserData, setSectionSelected,
                     <p className="text-base-content">{userData?.username}</p>
                 </div>
                 <div className="divider"></div>
-                {sectionSelected == 'settings' ? <button className="btn disabled w-full rounded-lg hover:cursor-not-allowed">Settings</button> : <button className="btn btn-primary w-full" onClick={handleSettingsSelected}>Settings</button>}
+                {sectionSelected == 'settings' ? <button className="btn disabled w-full rounded-lg hover:cursor-not-allowed">Settings</button> : <button className="btn btn-soft btn-primary w-full" onClick={handleSettingsSelected}>Settings</button>}
             </div>
             <UploadProfilePicture setUserData={setUserData} />
         </div>
